bd24fe5ef70e7ca81c26b84a320835b0306ed249
hefengbao <754582231@qq.com>
2018-03-07 19:56:13 +0800

    update

---
+++ src/Builder/ChartTemple.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace HeFengbao\Charts\Builder;
+
+use HeFengbao\Charts\Builder\Library\Chart;
+
+class ChartTemple extends Chart
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
